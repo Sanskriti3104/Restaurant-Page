fb0ff6e1939b10a67a62cfecdab15a64baf0f272
Sanskriti singh <146508510+Sanskriti3104@users.noreply.github.com>
2025-09-13 22:21:50 +0530

    Add Menu with its Styling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,23 @@ import './style.css';
 import homeImage from './images/home.jpg';
 document.getElementById('homeImage').src = homeImage;
 
+import MargheritaClassica from './images/ivan-torres-MQUqbmszGGM-unsplash.jpg';
+document.getElementById('MargheritaClassica').src = MargheritaClassica;
+
+import PepperoniPizza from './images/vit-ch-Oxb84ENcFfU-unsplash.jpg';
+document.getElementById('PepperoniPizza').src = PepperoniPizza;
+
+import VerdureGrigliate from './images/rahul-upadhyay-yDKHJxfiWDk-unsplash.jpg'
+document.getElementById('VerdureGrigliate').src = VerdureGrigliate;
+
+import SpaghettiAglioEOlio from './images/soham-chadha-IdR3W2mzueg-unsplash.jpg';
+document.getElementById('SpaghettiAglioEOlio').src = SpaghettiAglioEOlio;
+
+import PenneAlfredo from './images/alfredo-burgos-B9_pP3ZwX9g-unsplash.jpg';
+document.getElementById('PenneAlfredo').src = PenneAlfredo;
+
+import LasagnaAlForno from './images/pexels-shameel-mukkath-3421394-5640052.jpg';
+document.getElementById('LasagnaAlForno').src = LasagnaAlForno;
 const navBtns = document.querySelectorAll('.nav-btn');
 const tabContents = document.querySelectorAll('.tab-content');
 
